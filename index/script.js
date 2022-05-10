e2d27caeef0442d9290006ca923f6f7459bd3415
neodavis <dendhg.rgbg@gmail.com>
2022-05-10 20:18:22 +0300

    v4.8

--- index/script.js
+++ index/script.js
@@ -11,6 +11,12 @@ window.onload = function() {
     let navIcons = document.querySelectorAll('.material-icons')
     let themeSwitch = document.querySelector('.theme-switch')
     let themeSwitchFlag = false
+    let redirectArr = Array.from(document.querySelectorAll('.link-redirect'))
+    redirectArr.forEach((e)=>{
+        e.onclick = function() {
+            window.location.href = e.dataset.redirect
+        }
+    })
     linkArray.forEach((e)=>{
         linkArray.forEach((k)=>{
             k.classList.remove('active')
