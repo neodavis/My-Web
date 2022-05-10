485daa9b42518a462c81bf0c9875cee3d1c9a1f1
neodavis <dendhg.rgbg@gmail.com>
2022-05-11 00:24:24 +0300

    v6.0

--- index/script.js
+++ index/script.js
@@ -3,17 +3,34 @@ const swup = new Swup({
     options,
     plugins: [new SwupSlideTheme()]
 })
-
 window.onload = function() {
+    let themeSwitchFlag = false;
     let colorArray = document.querySelectorAll('.color-button')
     let colorSwitch = document.querySelector('.color-switch')
     let colorSwitchFlag = false;
     let linkArray = document.querySelectorAll('.navigation-item');
     let navIcons = document.querySelectorAll('.material-icons')
     let themeSwitch = document.querySelector('.theme-switch')
-    let themeSwitchFlag = false
-    let redirectArr = Array.from(document.querySelectorAll('.link-redirect'))
+    let rotate = 0;
 
+    if (localStorage.getItem('--font-color')) {
+        document.documentElement.style.setProperty('--font-color', localStorage.getItem('--font-color'))
+    }
+    if (localStorage.getItem('--general-font')) {
+        document.documentElement.style.setProperty('--general-font', localStorage.getItem('--general-font'))
+    }
+    if (localStorage.getItem('--background-color')) {
+        document.documentElement.style.setProperty('--background-color', localStorage.getItem('--background-color'))
+    }
+    if (localStorage.getItem('--background-lighten-color')) {
+        document.documentElement.style.setProperty('--background-lighten-color', localStorage.getItem('--background-lighten-color'))
+    }
+    if (localStorage.getItem('themeSwitchFlag')) {
+        themeSwitchFlag = JSON.parse(localStorage.getItem('themeSwitchFlag'))
+    }
+    if (localStorage.getItem('theme-switch-img')) {
+        document.querySelector('.theme-switch-img').src = localStorage.getItem('theme-switch-img')
+    }
     linkArray.forEach((e)=>{
         linkArray.forEach((k)=>{
             k.classList.remove('active')
@@ -91,20 +108,35 @@ window.onload = function() {
         }
     })
     themeSwitch.addEventListener('click', function() {
-        if (!themeSwitchFlag) {
-            document.querySelector('.theme-switch-img').style.transform = "rotate(720deg)"
-            document.querySelector('.theme-switch-img').src = "../index/assets/moon.png"
-            document.documentElement.style.setProperty('--background-lighten-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc'))
-            document.documentElement.style.setProperty('--background-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc-lighten'))
-            document.documentElement.style.setProperty('--general-font', '#2e314d')
+        if (rotate == 720) {
+            rotate = 0
         } else {
-            document.querySelector('.theme-switch-img').style.transform = "rotate(0deg)"
+            rotate = 720
+        }
+        if (themeSwitchFlag != false) {
+            document.querySelector('.theme-switch-img').style.transform = `rotate(${rotate}deg)`
+            document.querySelector('.theme-switch-img').src = "../index/assets/moon.png"
+            localStorage.setItem('theme-switch-img', "../index/assets/moon.png")
+            document.documentElement.style.setProperty('--background-lighten-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc-lighten'))
+            document.documentElement.style.setProperty('--background-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc'))
+            localStorage.setItem('--background-lighten-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc-lighten'))
+            localStorage.setItem('--background-color', getComputedStyle(document.documentElement).getPropertyValue('--light-bc'))
+            document.documentElement.style.setProperty('--general-font', '#2e314d')
+            localStorage.setItem('--general-font', '#2e314d')
+        } else {
+            document.querySelector('.theme-switch-img').style.transform = `rotate(${rotate}deg)`
             document.querySelector('.theme-switch-img').src = "../index/assets/sun.png"
+            localStorage.setItem('theme-switch-img', "../index/assets/sun.png")
             document.documentElement.style.setProperty('--background-lighten-color', getComputedStyle(document.documentElement).getPropertyValue('--dark-bc-lighten'))
             document.documentElement.style.setProperty('--background-color', getComputedStyle(document.documentElement).getPropertyValue('--dark-bc'))
+            localStorage.setItem('--background-lighten-color', getComputedStyle(document.documentElement).getPropertyValue('--dark-bc-lighten'))
+            localStorage.setItem('--background-color', getComputedStyle(document.documentElement).getPropertyValue('--dark-bc'))
             document.documentElement.style.setProperty('--general-font', '#fdfdff')
+            localStorage.setItem('--general-font', '#fdfdff')
         }
+        console.log(themeSwitchFlag)
         themeSwitchFlag = !themeSwitchFlag
+        localStorage.setItem('themeSwitchFlag', themeSwitchFlag)
     }, false)
 
     colorSwitch.children[0].addEventListener('click', function() {
@@ -120,6 +152,6 @@ window.onload = function() {
     colorArray.forEach((e)=>{
         e.addEventListener('click', function() {
             document.documentElement.style.setProperty('--font-color', e.dataset.color)
+            localStorage.setItem('--font-color', e.dataset.color)
         }, false)
-    })
-}    
+    })}
